d20af8b593d490c4c77ce7aad92817e09e990458
Agustinmiquel <agumiquel@.com>
2025-02-19 13:55:27 -0300

    products test jest 1 prueba

--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -1,18 +1,77 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Category } from 'src/categories/entities/category.entity';
+
+const mockRepositoryProduct = {
+  create: jest.fn().mockImplementation((dto) => dto),
+  save: jest
+    .fn()
+    .mockImplementation((product) =>
+      Promise.resolve({ id: Date.now(), ...product }),
+    ),
+  find: jest.fn().mockResolvedValue([]),
+  findOne: jest.fn().mockResolvedValue(null),
+};
+
+const mockCategoryRepository = {
+  findOne: jest.fn().mockResolvedValue(null),
+};
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  let repository: Repository<Product>;
+  let categoryRepository: Repository<Category>;
+  const productDto = {
+    name: 'product test',
+    description: 'description test',
+    price: 100,
+    stock: 10,
+    codigo: 31231,
+    categoryId: '1',
+    IsActive: true,
+    ImageProduct: 'https://www.google.com',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        {
+          provide: getRepositoryToken(Product),
+          useValue: mockRepositoryProduct,
+        },
+        {
+          provide: getRepositoryToken(Category),
+          useValue: mockCategoryRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
+    repository = module.get<Repository<Product>>(getRepositoryToken(Product));
+    categoryRepository = module.get<Repository<Category>>(
+      getRepositoryToken(Category),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  // crear un producto
+  it('should be created a product', () => {
+    const product = {
+      name: 'product test',
+      description: 'description test',
+      price: 100,
+      stock: 10,
+    };
+    expect(service.create(productDto)).toEqual({
+      id: expect.any(Number),
+      ...product,
+    });
   });
 });
