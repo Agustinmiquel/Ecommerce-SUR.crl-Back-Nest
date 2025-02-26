640b0a7ddf3cf962d125c3eaeeac2bc9a7b97002
Agustinmiquel <agumiquel@.com>
2025-02-26 01:48:41 -0300

    modificando el dto de products y la funcion findall

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -30,7 +30,6 @@ export class CreateProductDto {
 
   @IsString()
   @IsNotEmpty()
-  @IsPositive()
   categoryId: string;
 
   @IsBoolean()
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -24,6 +24,7 @@ export class ProductsService {
     return await this.productRepository.find({
       skip: skip,
       take: limit,
+      relations: ['category'],
     });
   }
 
